bfca8e95abe36d30690870c4562800a5b5beab48
Chaman Chaudhary <chaudharychaman1506@gmail.com>
2024-10-16 15:05:28 +0530

    resolving path

--- src/actions/problem.js
+++ src/actions/problem.js
@@ -4,7 +4,7 @@ import fs from "fs";
 import path from "path";
 import { LanguagesToExtension } from "./types";
 
-const MOUNT_PATH = path.resolve("src", "problems");
+const MOUNT_PATH = path.join(process.cwd(), "src", "problems");
 
 export const getProblem = async (problemSlug, languageId) => {
   languageId = LanguagesToExtension[languageId];
@@ -27,13 +27,7 @@ export const getProblem = async (problemSlug, languageId) => {
 async function getProblemFullBoilerplateCode(problemSlug, languageId) {
   return new Promise((resolve, reject) => {
     fs.readFile(
-      path.resolve(
-        MOUNT_PATH,
-        problemSlug,
-        "boilerplate-full",
-        `function.${languageId}`
-      ),
-      // `${MOUNT_PATH}/${problemSlug}/boilerplate-full/function.${languageId}`,
+      `${MOUNT_PATH}/${problemSlug}/boilerplate-full/function.${languageId}`,
       { encoding: "utf-8" },
       (err, data) => {
         if (err) {
@@ -48,8 +42,7 @@ async function getProblemFullBoilerplateCode(problemSlug, languageId) {
 async function getProblemInputs(problemSlug) {
   return new Promise((resolve, reject) => {
     fs.readdir(
-      path.resolve(MOUNT_PATH, problemSlug, "tests", `inputs`),
-      // `${MOUNT_PATH}/${problemSlug}/tests/inputs`,
+      `${MOUNT_PATH}/${problemSlug}/tests/inputs`,
       async (err, files) => {
         if (err) {
           console.log(err);
@@ -58,14 +51,7 @@ async function getProblemInputs(problemSlug) {
             files.map((file) => {
               return new Promise((resolve, reject) => {
                 fs.readFile(
-                  path.resolve(
-                    MOUNT_PATH,
-                    problemSlug,
-                    "tests",
-                    "inputs",
-                    `${file}`
-                  ),
-                  // `${MOUNT_PATH}/${problemSlug}/tests/inputs/${file}`,
+                  `${MOUNT_PATH}/${problemSlug}/tests/inputs/${file}`,
                   { encoding: "utf-8" },
                   (err, data) => {
                     if (err) {
@@ -90,8 +76,7 @@ async function getProblemInputs(problemSlug) {
 async function getProblemOutputs(problemSlug) {
   return new Promise((resolve, reject) => {
     fs.readdir(
-      path.resolve(MOUNT_PATH, problemSlug, "tests", "outputs"),
-      // `${MOUNT_PATH}/${problemSlug}/tests/outputs`,
+      `${MOUNT_PATH}/${problemSlug}/tests/outputs`,
       async (err, files) => {
         if (err) {
           console.log(err);
@@ -100,14 +85,7 @@ async function getProblemOutputs(problemSlug) {
             files.map((file) => {
               return new Promise((resolve, reject) => {
                 fs.readFile(
-                  path.resolve(
-                    MOUNT_PATH,
-                    problemSlug,
-                    "tests",
-                    "outputs",
-                    `${file}`
-                  ),
-                  // `${MOUNT_PATH}/${problemSlug}/tests/outputs/${file}`,
+                  `${MOUNT_PATH}/${problemSlug}/tests/outputs/${file}`,
                   { encoding: "utf-8" },
                   (err, data) => {
                     if (err) {
